7f6e4be091a06a601a16258854be3da7ed763ac4
Monica  Karasek <mlkarasek@gmail.com>
2019-11-22 21:05:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(list, name){
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase( === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(list, partialName) {
+  let nameLength = partialName.length; 
+  return list.filter(function (driverName) {
+    return driverName.slice(0, nameLength) === partialName;
+  });
+}
+
+function matchName(list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
